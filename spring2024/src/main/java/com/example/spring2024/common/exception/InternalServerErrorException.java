117d755862b11e5a879507760c6bb9932c8da22f
lejuho <zedd1324@gmail.com>
2024-11-22 15:16:11 +0900

    hw8 commit

--- spring2024/src/main/java/com/example/spring2024/common/exception/InternalServerErrorException.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring2024.common.exception;
-
-public class InternalServerErrorException extends RuntimeException {
-    public InternalServerErrorException(String message) {
-        super(message);
-    }
-}
